b1bc2024728cd15efd036d5d51d7cb9b1cf74494
Aldi-H <aldihimawan88@gmail.com>
2024-10-26 08:25:44 +0700

    Revert "Refactor dropdown component and fix table component import"
    This reverts commit 8b0aa2129dcef8282e528e2583571bdf881cc329.

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
+import { CloseCircle } from "iconsax-react";
+import Select from "react-select"; // Import react-select
+import colors from "../styles/colors";
 
-import Select from "react-select";
-
-const Dropdown = ({ options, placeholder = "Pilih Opsi", onSelect, value }) => {
+const Dropdown = ({
+  options,
+  label,
+  placeholder = "Pilih Opsi",
+  onSelect,
+  value,
+  isRequired = false,
+}) => {
   const [selectedValue, setSelectedValue] = useState(value || null);
+  const [error, setError] = useState("");
 
   // Map options to format required by react-select
   const formattedOptions = options.map((option) => ({
@@ -14,23 +23,50 @@ const Dropdown = ({ options, placeholder = "Pilih Opsi", onSelect, value }) => {
   const handleChange = (selectedOption) => {
     setSelectedValue(selectedOption);
     onSelect(selectedOption);
+
+    if (isRequired && !selectedOption) {
+      setError("This field is required");
+    } else {
+      setError("");
+    }
   };
 
   return (
     <div className="relative flex flex-col w-full">
+      {" "}
+      {/* Added p-6 here */}
+      {label && (
+        <label className="text-B2 text-emphasis-on_surface-high mb-1">
+          {label}
+          {isRequired && <span className="text-custom-red-500"> *</span>}
+        </label>
+      )}
       <Select
         value={selectedValue}
         onChange={handleChange}
         options={formattedOptions}
         placeholder={placeholder}
+        className={`react-select ${
+          error
+            ? "border-custom-red-500"
+            : "border border-surface-light-outline"
+        }`}
         classNamePrefix="select" // Prefix for styling
         isClearable={false} // Disable clearing for a required field
         styles={{
           control: (base) => ({
             ...base,
             minHeight: "48px", // Match min height
+            borderWidth: "1.5px", // Border width
+            borderRadius: "16px", // Border radius
             transition: "all 200ms ease-in-out", // Transition effect
-            borderRadius: "16px",
+            borderColor: error ? colors.Solid.Basic.Red[500] : base.borderColor,
+            boxShadow: "none",
+            "&:hover": {
+              borderColor: error
+                ? colors.Solid.Basic.Red[500]
+                : colors.Solid.Basic.Blue[500], // Change color on hover
+            },
           }),
           placeholder: (base) => ({
             ...base,
@@ -51,6 +87,17 @@ const Dropdown = ({ options, placeholder = "Pilih Opsi", onSelect, value }) => {
           }),
         }}
       />
+      {error && (
+        <div className="flex items-center mt-1">
+          <CloseCircle
+            color={colors.Solid.Basic.Red[500]}
+            variant="Linear"
+            size={16}
+            className="mr-1"
+          />
+          <span className="text-custom-red-500 text-ExtraSmall">{error}</span>
+        </div>
+      )}
     </div>
   );
 };
